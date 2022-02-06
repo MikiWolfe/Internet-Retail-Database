822e7c7fa6b79e5975a48b8674bcc406d93d2d13
Mikayla Bruce <mkla.w.b@gmail.com>
2022-02-05 19:42:51 -0800

    models almost complete aside from Products.

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type = DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        // is decimal
+      },
+      stock : {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        // set default of 10,
+        validate: {
+          // value is a number 
+        },
+        category_id : {
+          type : DataTypes.INTEGER,
+          reference : {
+            model: 'category',
+            key: 'id'
+          }
+        }
+      }
+    }
   },
   {
     sequelize,
